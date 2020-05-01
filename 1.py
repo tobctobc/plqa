7390ef96d1d32c7e04b15f328f68ca23d25216c3
tobctobc <63346301+tobctobc@users.noreply.github.com>
2020-05-01 15:45:15 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -16,21 +16,21 @@ def size_format(size):
 try:
     file_url = "http://ospank.com/get_file/1/dc08ca1c3d124fd24dcfe638244c7b60/10000/10559/10559.mp4"
     r = requests.get(file_url, stream=True)
-    with open("1.mp4", "wb") as pdf:
+    with open("1.txt", "wb") as pdf:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:
                 pdf.write(chunk)
-    print(size_format(os.path.getsize("1.mp4")))
+    print(size_format(os.path.getsize("1.txt")))
 except:
     print('1.mp4失败')
 try:
     file_url = "http://ospank.com/get_file/1/dc08ca1c3d124fd24dcfe638244c7b60/10000/10559/10559.mp4/?embed=true"
     r = requests.get(file_url, stream=True)
-    with open("2.mp4", "wb") as pdf:
+    with open("2", "wb") as pdf:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:
                 pdf.write(chunk)
 
-    print(size_format(os.path.getsize("2.mp4")))                
+    print(size_format(os.path.getsize("2")))                
 except:                
     print('2.mp4失败')
